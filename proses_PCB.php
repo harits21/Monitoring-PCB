180843571ddf3cfb4cee3d24dbe75521a61fc576
Muhammad Harits Rasyid Ramadhan <rasyid.haris@gmail.com>
2021-08-05 10:31:18 +0700

    Menambahkan template karyawan

--- proses_PCB.php
+++ proses_PCB.php
@@ -1,184 +1,166 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php include_once('_templateKaryawan.php') ?>
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Karyawan</title>
-
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
-
-</head>
-
-<body>
-    <!-- NAVBAR -->
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="container">
-            <a class="navbar-brand" href="karyawan.php">RAFTECH PCB</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                <div class="navbar-nav">
-                    <a class="nav-link active" href="">Karyawan</a>
-
-                </div>
-            </div>
-        </div>
-    </nav>
-    <!-- END OF NAVBAR -->
-
-    <!-- CONTENT -->
+<!-- NAVBAR -->
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container">
-        <div class="row">
-            <div class="col-8">
-                <h2 class="my-3">Proses Pembuatan PCB</h2>
-                <form action="" method="POST">
-                    <div class="row mb-3">
-                        <label for="" class="col-sm-2 col-form-label">Nama Customer</label>
-                        <div class="col-sm-10">
-                            <select class="form-select" aria-label="Default select example">
-                                <option selected>--Pilih Nama Customer--</option>
-                                <option value="1">One</option>
-                                <option value="2">Two</option>
-                                <option value="3">Three</option>
-                            </select>
-                        </div>
-                    </div>
-                    <div class="row mb-3">
-                        <label for="" class="col-sm-2 col-form-label">No Invoice</label>
-                        <div class="col-sm-10">
-                            <select class="form-select" aria-label="Default select example">
-                                <option selected>--Pilih No Invoice--</option>
-                                <option value="1">One</option>
-                                <option value="2">Two</option>
-                                <option value="3">Three</option>
-                            </select>
-                        </div>
-                    </div>
-                    <div class="row mb-3">
-                        <label for="" class="col-sm-2 col-form-label">Spec PCB</label>
-                        <div class="col-sm-10">
-                            <input type="text" class="form-control" id="" name="">
-                        </div>
-                    </div>
-                    <div class="row mb-3">
-                        <label for="" class="col-sm-2 col-form-label">Jumlah</label>
-                        <div class="col-sm-10">
-                            <input type="number" class="form-control" id="" name="">
-                        </div>
-                    </div>
-                    <div class="row mb-3">
-                        <label for="" class="col-sm-2 col-form-label">Nama Karyawan</label>
-                        <div class="col-sm-10">
-                            <select class="form-select" aria-label="Default select example">
-                                <option selected>--Pilih Karyawan--</option>
-                                <option value="1">One</option>
-                                <option value="2">Two</option>
-                                <option value="3">Three</option>
-                            </select>
-                        </div>
-                    </div>
-                    <div class="row mb-3">
-                        <label for="" class="col-sm-2 col-form-label">Order Verified</label>
-                        <div class="col-sm-10">
-                            <a href="" class="btn btn-primary">Start</a>
-                            <a href="" class="btn btn-success">Stop</a>
-                        </div>
-                    </div>
-                    <div class="row mb-3">
-                        <label for="" class="col-sm-2 col-form-label">Potong PCB</label>
-                        <div class="col-sm-10">
-                            <a href="" class="btn btn-primary">Start</a>
-                            <a href="" class="btn btn-success">Stop</a>
-                        </div>
-                    </div>
-                    <div class="row mb-3">
-                        <label for="" class="col-sm-2 col-form-label">Cetak Jalur bawah</label>
-                        <div class="col-sm-10">
-                            <a href="" class="btn btn-primary">Start</a>
-                            <a href="" class="btn btn-success">Stop</a>
-                        </div>
-                    </div>
-                    <div class="row mb-3">
-                        <label for="" class="col-sm-2 col-form-label">Cetak jalur Atas</label>
-                        <div class="col-sm-10">
-                            <a href="" class="btn btn-primary">Start</a>
-                            <a href="" class="btn btn-success">Stop</a>
-                        </div>
-                    </div>
-                    <div class="row mb-3">
-                        <label for="" class="col-sm-2 col-form-label">Masking Bawah</label>
-                        <div class="col-sm-10">
-                            <a href="" class="btn btn-primary">Start</a>
-                            <a href="" class="btn btn-success">Stop</a>
-                        </div>
-                    </div>
-                    <div class="row mb-3">
-                        <label for="" class="col-sm-2 col-form-label">Masking Atas</label>
-                        <div class="col-sm-10">
-                            <a href="" class="btn btn-primary">Start</a>
-                            <a href="" class="btn btn-success">Stop</a>
-                        </div>
-                    </div>
-                    <div class="row mb-3">
-                        <label for="" class="col-sm-2 col-form-label">Silkscreen Bawah</label>
-                        <div class="col-sm-10">
-                            <a href="" class="btn btn-primary">Start</a>
-                            <a href="" class="btn btn-success">Stop</a>
-                        </div>
-                    </div>
-                    <div class="row mb-3">
-                        <label for="" class="col-sm-2 col-form-label">Silkscreen Atas</label>
-                        <div class="col-sm-10">
-                            <a href="" class="btn btn-primary">Start</a>
-                            <a href="" class="btn btn-success">Stop</a>
-                        </div>
-                    </div>
-                    <div class="row mb-3">
-                        <label for="" class="col-sm-2 col-form-label">Bor</label>
-                        <div class="col-sm-10">
-                            <a href="" class="btn btn-primary">Start</a>
-                            <a href="" class="btn btn-success">Stop</a>
-                        </div>
-                    </div>
-                    <div class="row mb-3">
-                        <label for="" class="col-sm-2 col-form-label">Plating</label>
-                        <div class="col-sm-10">
-                            <a href="" class="btn btn-primary">Start</a>
-                            <a href="" class="btn btn-success">Stop</a>
-                        </div>
-                    </div>
-                    <div class="row mb-3">
-                        <label for="" class="col-sm-2 col-form-label">Finishing</label>
-                        <div class="col-sm-10">
-                            <a href="" class="btn btn-primary">Start</a>
-                            <a href="" class="btn btn-success">Stop</a>
-                        </div>
-                    </div>
-                    <div class="row mb-3">
-                        <label for="" class="col-sm-2 col-form-label">Quality Control</label>
-                        <div class="col-sm-10">
-                            <a href="" class="btn btn-primary">Start</a>
-                            <a href="" class="btn btn-success">Stop</a>
-                        </div>
-                    </div>
-                    <div class="row mb-3">
-                        <label for="" class="col-sm-2 col-form-label">Packing</label>
-                        <div class="col-sm-10">
-                            <a href="" class="btn btn-primary">Start</a>
-                            <a href="" class="btn btn-success">Stop</a>
-                        </div>
-                    </div>
-                </form>
+        <a class="navbar-brand" href="karyawan.php">RAFTECH PCB</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+                <a class="nav-link active" href="">Karyawan</a>
+
             </div>
         </div>
     </div>
-    <!-- END OF CONTENT -->
+</nav>
+<!-- END OF NAVBAR -->
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
-</body>
-
-</html>
+<!-- CONTENT -->
+<div class="container">
+    <div class="row">
+        <div class="col-8">
+            <h2 class="my-3">Proses Pembuatan PCB</h2>
+            <form action="" method="POST">
+                <div class="row mb-3">
+                    <label for="" class="col-sm-2 col-form-label">Nama Customer</label>
+                    <div class="col-sm-10">
+                        <select class="form-select" aria-label="Default select example">
+                            <option selected>--Pilih Nama Customer--</option>
+                            <option value="1">One</option>
+                            <option value="2">Two</option>
+                            <option value="3">Three</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="row mb-3">
+                    <label for="" class="col-sm-2 col-form-label">No Invoice</label>
+                    <div class="col-sm-10">
+                        <select class="form-select" aria-label="Default select example">
+                            <option selected>--Pilih No Invoice--</option>
+                            <option value="1">One</option>
+                            <option value="2">Two</option>
+                            <option value="3">Three</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="row mb-3">
+                    <label for="" class="col-sm-2 col-form-label">Spec PCB</label>
+                    <div class="col-sm-10">
+                        <input type="text" class="form-control" id="" name="">
+                    </div>
+                </div>
+                <div class="row mb-3">
+                    <label for="" class="col-sm-2 col-form-label">Jumlah</label>
+                    <div class="col-sm-10">
+                        <input type="number" class="form-control" id="" name="">
+                    </div>
+                </div>
+                <div class="row mb-3">
+                    <label for="" class="col-sm-2 col-form-label">Nama Karyawan</label>
+                    <div class="col-sm-10">
+                        <select class="form-select" aria-label="Default select example">
+                            <option selected>--Pilih Karyawan--</option>
+                            <option value="1">One</option>
+                            <option value="2">Two</option>
+                            <option value="3">Three</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="row mb-3">
+                    <label for="" class="col-sm-2 col-form-label">Order Verified</label>
+                    <div class="col-sm-10">
+                        <a href="" class="btn btn-primary">Start</a>
+                        <a href="" class="btn btn-success">Stop</a>
+                    </div>
+                </div>
+                <div class="row mb-3">
+                    <label for="" class="col-sm-2 col-form-label">Potong PCB</label>
+                    <div class="col-sm-10">
+                        <a href="" class="btn btn-primary">Start</a>
+                        <a href="" class="btn btn-success">Stop</a>
+                    </div>
+                </div>
+                <div class="row mb-3">
+                    <label for="" class="col-sm-2 col-form-label">Cetak Jalur bawah</label>
+                    <div class="col-sm-10">
+                        <a href="" class="btn btn-primary">Start</a>
+                        <a href="" class="btn btn-success">Stop</a>
+                    </div>
+                </div>
+                <div class="row mb-3">
+                    <label for="" class="col-sm-2 col-form-label">Cetak jalur Atas</label>
+                    <div class="col-sm-10">
+                        <a href="" class="btn btn-primary">Start</a>
+                        <a href="" class="btn btn-success">Stop</a>
+                    </div>
+                </div>
+                <div class="row mb-3">
+                    <label for="" class="col-sm-2 col-form-label">Masking Bawah</label>
+                    <div class="col-sm-10">
+                        <a href="" class="btn btn-primary">Start</a>
+                        <a href="" class="btn btn-success">Stop</a>
+                    </div>
+                </div>
+                <div class="row mb-3">
+                    <label for="" class="col-sm-2 col-form-label">Masking Atas</label>
+                    <div class="col-sm-10">
+                        <a href="" class="btn btn-primary">Start</a>
+                        <a href="" class="btn btn-success">Stop</a>
+                    </div>
+                </div>
+                <div class="row mb-3">
+                    <label for="" class="col-sm-2 col-form-label">Silkscreen Bawah</label>
+                    <div class="col-sm-10">
+                        <a href="" class="btn btn-primary">Start</a>
+                        <a href="" class="btn btn-success">Stop</a>
+                    </div>
+                </div>
+                <div class="row mb-3">
+                    <label for="" class="col-sm-2 col-form-label">Silkscreen Atas</label>
+                    <div class="col-sm-10">
+                        <a href="" class="btn btn-primary">Start</a>
+                        <a href="" class="btn btn-success">Stop</a>
+                    </div>
+                </div>
+                <div class="row mb-3">
+                    <label for="" class="col-sm-2 col-form-label">Bor</label>
+                    <div class="col-sm-10">
+                        <a href="" class="btn btn-primary">Start</a>
+                        <a href="" class="btn btn-success">Stop</a>
+                    </div>
+                </div>
+                <div class="row mb-3">
+                    <label for="" class="col-sm-2 col-form-label">Plating</label>
+                    <div class="col-sm-10">
+                        <a href="" class="btn btn-primary">Start</a>
+                        <a href="" class="btn btn-success">Stop</a>
+                    </div>
+                </div>
+                <div class="row mb-3">
+                    <label for="" class="col-sm-2 col-form-label">Finishing</label>
+                    <div class="col-sm-10">
+                        <a href="" class="btn btn-primary">Start</a>
+                        <a href="" class="btn btn-success">Stop</a>
+                    </div>
+                </div>
+                <div class="row mb-3">
+                    <label for="" class="col-sm-2 col-form-label">Quality Control</label>
+                    <div class="col-sm-10">
+                        <a href="" class="btn btn-primary">Start</a>
+                        <a href="" class="btn btn-success">Stop</a>
+                    </div>
+                </div>
+                <div class="row mb-3">
+                    <label for="" class="col-sm-2 col-form-label">Packing</label>
+                    <div class="col-sm-10">
+                        <a href="" class="btn btn-primary">Start</a>
+                        <a href="" class="btn btn-success">Stop</a>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+<!-- END OF CONTENT -->
