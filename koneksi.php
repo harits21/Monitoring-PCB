b05dfcceeddc22f622fa046cdd077c62af2fada0
Muhammad Harits Rasyid Ramadhan <rasyid.haris@gmail.com>
2021-08-20 14:14:54 +0700

    update db remote

--- koneksi.php
+++ koneksi.php
@@ -1,1 +1,6 @@
-<?php $koneksi = new mysqli("localhost", "root", "", "pcb");
+<?php
+$servername = "mysql.hostinger.co.id";
+$database = "u541361579_pcb";
+$usernamedb = "u541361579_raftechpcb";
+$passworddb = "Bandungpcb2021";
+$koneksi = mysqli_connect($servername, $usernamedb, $passworddb, $database);
